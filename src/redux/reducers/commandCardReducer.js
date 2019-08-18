ede73a7a9347c9c2c6e08790103b0a6a17780ccd
kenneth-poling <kenneth-poling@teletracking.com>
2019-08-18 12:10:17 -0400

    Removes log

--- src/redux/reducers/commandCardReducer.js
+++ src/redux/reducers/commandCardReducer.js
@@ -5,7 +5,6 @@ const initialState = {
 };
 
 export default function(state = initialState, action){
-  console.log(action);
   switch(action.type){
     case SAVE_COMMAND_CARD_CONFIG:
       return {
