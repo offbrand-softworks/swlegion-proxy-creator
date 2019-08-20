51194470d0a962727a7f528c5b1d4ae139ba7ed6
Brandon Horn <brandon.thomas.horn@gmail.com>
2019-08-20 15:11:45 -0400

    initial render of command card and accompanying css

---
+++ src/assets/images.js
@@ -0,0 +1,9 @@
+const commandImages = [
+  { id: "onePip", src: "../../../images/command/1pip.png" },
+  { id: "twoPip", src: "./command/2pip.png" },
+  { id: "threePip", src: "./command/3pip.png" },
+  { id: "fourPip", src: "./command/4pip.png" },
+  { id: "commanderTemplate", src: "./command/commanderTemplate.png" }
+];
+
+export default commandImages;
